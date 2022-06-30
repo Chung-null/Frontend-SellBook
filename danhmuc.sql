b70cb973c0c7bf117842ed8e34ae74282035e863
ChungDang <chungptkrb@gmail.com>
2022-06-30 23:35:00 +0700

    Update Frontend Edubook

--- danhmuc.sql
+++
@@ -1,9 +0,0 @@
-BULK INSERT danhmuc
-FROM 'C:\Users\chung\OneDrive\Desktop\EduBook (1)\EduBook\danhmuc.csv'
-WITH
-(
-    FIRSTROW = 2,
-    FIELDTERMINATOR = ',',  --CSV field delimiter
-    ROWTERMINATOR = '\n',   --Use to shift the control to next row
-    TABLOCK
-)
